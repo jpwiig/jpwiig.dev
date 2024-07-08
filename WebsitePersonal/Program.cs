7d64dbb9fa8c71e52cbfa9d9e07f5fddc320f18d
jpwiig <jpwiig@gmail.com>
2024-07-08 07:56:50 +0200

    started on login logic

--- WebsitePersonal/Program.cs
+++ WebsitePersonal/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp1;
+using Kinde.Api.Models.Configuration;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddTransient<IAuthorizationConfigurationProvider, DefaultAuthorizationConfigurationProvider>();
+builder.Services.AddTransient<IApplicationConfigurationProvider, DefaultApplicationConfigurationProvider>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
 await builder.Build().RunAsync();
---
+++ WebsitePersonal/models/Cve.cs
@@ -0,0 +1,8 @@
+namespace BlazorApp1.models;
+
+public class Cve
+{
+  
+    private String id { get; set; }
+    private object [] change { get; set; }
+}
